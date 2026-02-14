6a71cf944c7605a779cea7266eae7f1dce40aae8
Carlos Galindo <c.galindo.islas@gmail.com>
2026-02-14 17:06:54 -0600

    feat: create counter button

---
+++ src/app/pages/counter/counter.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from "@angular/core";
+
+@Component({
+  templateUrl: './counter.html',
+  styleUrl: './counter.css'
+})
+export class Counter {
+  protected number = signal(0);
+
+  increaseValue = (value: number) => {
+    this.number.update(current => current + value)
+  }
+
+  resetCounter = () => {
+    this.number.update(() => 0)
+  }
+}
