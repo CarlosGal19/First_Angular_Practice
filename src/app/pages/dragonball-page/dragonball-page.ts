c291a2d1a0b97878ec422a13b8eaf246c9798f58
Carlos Galindo <c.galindo.islas@gmail.com>
2026-02-15 16:14:58 -0600

    feat: render list from signals

--- src/app/pages/dragonball-page/dragonball-page.ts
+++ src/app/pages/dragonball-page/dragonball-page.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, WritableSignal } from '@angular/core';
+
+interface ICharacter {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball-page',
@@ -6,5 +12,33 @@ import { Component } from '@angular/core';
   templateUrl: './dragonball-page.html',
 })
 export class DragonballPage {
+  protected characters: WritableSignal<ICharacter[]> = signal([
+    {
+      id: 1,
+      name: 'Goku',
+      power: 9500,
+    },
+    {
+      id: 2,
+      name: 'Vegeta',
+      power: 9000,
+    },
+    {
+      id: 3,
+      name: 'Gohan',
+      power: 8700,
+    },
+    {
+      id: 4,
+      name: 'Piccolo',
+      power: 8200,
+    }
+  ])
 
+  // protected powerClasses = computed(() => {
+  //   return {
+  //     'text-danger': true,
+  //     'text-primary': true,
+  //   };
+  // })
 }
