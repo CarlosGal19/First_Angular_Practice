313df0499473299007c3889cf4be4f7f811684df
Carlos Galindo <c.galindo.islas@gmail.com>
2026-02-21 11:44:52 -0600

    fix: move nav bar route importation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBar } from './nav-bar/nav-bar';
+import { NavBar } from './components/nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
