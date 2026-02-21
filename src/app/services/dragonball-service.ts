c79357ffb949a7006fd8deaf9c6c0928419ea9c4
Carlos Galindo <c.galindo.islas@gmail.com>
2026-02-21 13:03:45 -0600

    feat: save characters in local storage

--- src/app/services/dragonball-service.ts
+++ src/app/services/dragonball-service.ts
@@ -1,15 +1,21 @@
-import { Injectable, signal, WritableSignal } from '@angular/core';
+import { effect, Injectable, signal, WritableSignal } from '@angular/core';
 import { ICharacter } from '../interfaces/character.interface';
+
+const loadCharacters = (): ICharacter[] => {
+  const characters = JSON.parse(localStorage.getItem('characters') ?? '[]') as ICharacter[]
+
+  return [];
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class DragonballService {
-  characters: WritableSignal<ICharacter[]> = signal([{
-    id: 1,
-    name: 'Goku',
-    power: 9001
-  }])
+  characters: WritableSignal<ICharacter[]> = signal(loadCharacters())
+
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem('db-super-characters', JSON.stringify(this.characters()));
+  })
 
   addCharacter = (newCharacter: ICharacter) => {
     const { id, name, power } = newCharacter;
